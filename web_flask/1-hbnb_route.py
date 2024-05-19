d97aef8c2cbf36031cf94027f9eb5bed1123b327
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-05-19 01:57:16 -0700

    a

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 0. Script to start a Flask web application """
+""" 1. Script to start a Flask web application with 2 commands """
 
 from flask import Flask
 
@@ -11,6 +11,12 @@ app = Flask(__name__)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
